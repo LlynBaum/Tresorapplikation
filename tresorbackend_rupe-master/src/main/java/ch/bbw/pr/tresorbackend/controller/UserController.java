84d3dd86fa374ca08b9a861f28a7cd63b9d1b0eb
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2025-05-16 08:50:56 +0200

    Update UserController.java

--- tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/controller/UserController.java
+++ tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/controller/UserController.java
@@ -22,7 +22,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
